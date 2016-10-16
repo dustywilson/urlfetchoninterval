5bf38ea62e121b8091662ee9502045d420009c86
Dusty Wilson <dusty.wilson@scjalliance.com>
2016-10-15 23:11:27 -0700

    It works

---
+++ exec.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+HTTP_HOST=${HTTP_HOST:-$SITE_PORT_80_TCP_ADDR}
+
+if [ -z "$SITE_PORT_80_TCP_ADDR" ]; then
+	echo "You must --link \$CONTAINERNAME:site or this is useless."
+	exit 1
+fi
+
+if [ "$HTTP_HOST" != "$SITE_PORT_80_TCP_ADDR" ]; then
+	echo "$SITE_PORT_80_TCP_ADDR $HTTP_HOST" >> /etc/hosts
+fi
+
+while true; do
+	sleep 60s
+	wget -O/dev/null --header="X-Forwarded-Email: dusty.wilson@scjalliance.com" --header="X-Forwarded-User: dusty.wilson" "http://$HTTP_HOST$GET_PATH"
+done
